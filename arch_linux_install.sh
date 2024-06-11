edde09637c47f946739d7460af89e895978ef14a
Gianmarco <giangenialus42@gmail.com>
2024-06-11 14:56:08 +0200

    added rofi config

--- arch_linux_install.sh
+++ arch_linux_install.sh
@@ -1,16 +1,30 @@
+
+#packages
 sudo pacman -Syu
 sudo pacman -S git hyprland waybar rio sddm micro dunst alacritty bat zsh python3 rofi
+
+#home
 cp my_home/.bashrc $HOME/.bashrc
 cp my_home/.colors $HOME/.colors
 cp my_home/.zshrc $HOME/.zshrc
+
+#rofi
+cp -r my_home/.local/share/rofi $HOME/.local/share/
+
+#configs
 cd my_home/.config
 cp -r waybar $HOME/.config
 cp -r rio $HOME/.config
-cp -r hypr $HOME/.config
+cp -r hypr $HOME/.config  
+
+#zsh
 cd /usr/share
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 sudo git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git
 sudo git clone https://github.com/agkozak/zsh-z.git
 sudo git clone https://github.com/zsh-users/zsh-history-substring-search.git
+sudo chsh
+
+#reboot
 sudo reboot
